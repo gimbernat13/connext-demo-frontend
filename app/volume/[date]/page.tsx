7822b095246cd66e7950acfdbb4fcdc21a3c64a1
Diego <gimbernat13@gmail.com>
2024-04-24 23:59:43 -0600

    Update page.tsx

--- app/volume/[date]/page.tsx
+++ app/volume/[date]/page.tsx
@@ -1,17 +1,22 @@
 import { Suspense } from "react";
 import Volume from "./volume";
 import Link from "next/link";
-import pepe from "../../../public/download.gif"
 import Image from "next/image";
+import pepe from "../../../public/download.gif";
+
 export default function Page({ params }: { params: { date: string } }) {
-  const formatDate = (date : Date) => {
+  const formatDate = (date: Date) => {
     return date.toISOString().split('T')[0];
   }
 
-  const getDateDaysAgo = (days : number) => {
+  const getDateDaysAgo = (days: number) => {
     const date = new Date();
     date.setDate(date.getDate() - days);
     return formatDate(date);
+  };
+
+  const isActive = (days: number) => {
+    return params.date === getDateDaysAgo(days);
   };
 
   return (
@@ -19,36 +24,13 @@ export default function Page({ params }: { params: { date: string } }) {
       <h1 className='p-4 text-4xl text-center'>Connext Daily Transfers</h1>
 
       <div className="flex gap-2 my-4 justify-center">
-        <Link href={`/volume/${getDateDaysAgo(7)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-            7 Days
-          </div>
-        </Link>
-        <Link href={`/volume/${getDateDaysAgo(15)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-            15 Days
-          </div>
-        </Link>
-        <Link href={`/volume/${getDateDaysAgo(30)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-            30 Days
-          </div>
-        </Link>
-        <Link href={`/volume/${getDateDaysAgo(90)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-            90 Days
-          </div>
-        </Link>
-        <Link href={`/volume/${getDateDaysAgo(365)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-            1 year 
-          </div>
-        </Link>
-        <Link href={`/volume/${getDateDaysAgo(600)}`}>
-          <div className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer">
-           2 years
-          </div>
-        </Link>
+        {[7, 15, 30, 90, 365, 600].map((days) => (
+          <Link href={`/volume/${getDateDaysAgo(days)}`} key={days}>
+            <div className={`bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer ${isActive(days) ? "ring-2 ring-white" : ""}`}>
+              {days} Days
+            </div>
+          </Link>
+        ))}
       </div>
 
       <Suspense fallback={
