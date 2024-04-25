7b00426067e1b9adcc426ceee720c614897d4617
Diego <gimbernat13@gmail.com>
2024-04-25 00:56:33 -0600

    updated css

--- app/volume/[date]/page.tsx
+++ app/volume/[date]/page.tsx
@@ -20,17 +20,18 @@ export default function Page({ params }: { params: { date: string } }) {
   };
 
   return (
-    <>
-      <h1 className='p-4 text-4xl text-center'>Connext Daily Transfers</h1>
-
-      <div className="flex gap-2 my-4 justify-center">
-        {[7, 15, 30, 90, 365, 600].map((days) => (
-          <Link href={`/volume/${getDateDaysAgo(days)}`} key={days}>
-            <div className={`bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer ${isActive(days) ? "ring-2 ring-white" : ""}`}>
-              {days} Days
-            </div>
-          </Link>
-        ))}
+    <div className="container mx-auto max-w-6xl p-4 bg-black">
+      <div className="flex justify-between items-center my-4">
+        <h1 className='text-lg p-4'>Connext Daily Transfers</h1>
+        <div className="flex gap-2 p-4">
+          {[7, 15, 30, 90, 365, 600].map((days) => (
+            <Link href={`/volume/${getDateDaysAgo(days)}`} key={days}>
+              <div className={`bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-sm cursor-pointer ${isActive(days) ? "ring-2 ring-white" : ""}`}>
+                {days} Days
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
 
       <Suspense fallback={
@@ -40,6 +41,6 @@ export default function Page({ params }: { params: { date: string } }) {
       </div>}>
         <Volume date={params.date} />
       </Suspense>
-    </>
+    </div>
   );
 }
