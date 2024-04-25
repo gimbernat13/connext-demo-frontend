b53abe1615b8cd0e3b6e78f43a11eef158ac6f20
Diego <gimbernat13@gmail.com>
2024-04-25 02:07:16 -0600

    Update chainDetails.ts

--- app/components/VolumeChart/chainDetails.ts
+++ app/components/VolumeChart/chainDetails.ts
@@ -1,11 +1,11 @@
 export const chainDetails = {
-  '6648936': { color: "#1A936F", name: 'Ethereum' }, // Strong, vibrant green
-  '1886350457': { color: '#663399', name: 'Polygon' }, // Deep purple
-  '1869640809': { color: '#E63946', name: 'Optimism' }, // Bright red
-  '1634886255': { color: '#2a9d8f', name: 'Arbitrum' }, // Teal green
-  '6778479': { color: '#2a623d', name: 'Gnosis' }, // Darker green
-  '6450786': { color: '#0077b6', name: 'BNB' }, // Strong blue
-  '1818848877': { color: '#e76f51', name: 'Linea' }, // Vibrant orange (if you prefer another strong color, this can be adjusted)
-  '1835365481': { color: '#560bad', name: 'Metis' }, // Royal purple
-  '1650553709': { color: '#d62828', name: 'Base' } // Vivid red
+  '6648936': { color: "#034638", name: 'Ethereum' }, // Deep green
+  '1886350457': { color: '#726a95', name: 'Polygon' }, // Muted purple
+  '1869640809': { color: '#8d6262', name: 'Optimism' }, // Muted red
+  '1634886255': { color: '#39603d', name: 'Arbitrum' }, // Forest green
+  '6778479': { color: '#a2c5ac', name: 'Gnosis' }, // Soft green
+  '6450786': { color: '#e0b589', name: 'BNB' }, // Sepia
+  '1818848877': { color: '#3d5a6c', name: 'Linea' }, // Muted teal
+  '1835365481': { color: '#1d1e2c', name: 'Metis' }, // Dark blue
+  '1650553709': { color: '#ab987a', name: 'Base' } // Muted gold
 };
